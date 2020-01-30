1f2b0da1b65654c74611b82844d7215d4f6ec1b7
MVRigonati <marcus.rigonati@hotmail.com>
2020-01-30 16:43:21 -0300

    Criando Componente Agenda

--- App.js
+++ App.js
@@ -1,30 +1,15 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import commonStyles, { loadCustomFont } from './src/commonStyles';
+import React, { Component } from 'react'
+import { loadCustomFont } from './src/commonStyles'
+import Agenda from './src/screens/Agenda'
 
 export default class App extends Component {
-
 	componentDidMount() {
 		loadCustomFont()
 	}
 
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text style={styles.welcome}>Open up App.js to start working on your app!</Text>
-			</View>
+			<Agenda />
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-	welcome: {
-		fontFamily: commonStyles.fontFamily
-	},
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-});
