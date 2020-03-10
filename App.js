d40eb7b7c36129d9abe923e2dbdef5ea06563cc6
MVRigonati <marcus.rigonati@hotmail.com>
2020-03-10 13:42:31 -0300

    Adicionando tempo de carregamento para a fonte customizada

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
+import * as Font from 'expo-font'
 import React, { Component } from 'react'
-import { loadCustomFont } from './src/commonStyles'
 import Auth from './src/screens/Auth'
+import { AppLoading } from 'expo'
 
 export default class App extends Component {
-	componentDidMount() {
-		loadCustomFont()
+	state = {
+		showLoadingScreen: true
 	}
 
-	render() {
-		return <Auth />
+	loadCustomFont = async () => {
+		await Font.loadAsync({
+			lato: require('./assets/fonts/Lato.ttf')
+		})
+	}
+
+	loadingScreen = () => {
+		return (
+			<AppLoading
+				onError={console.warn}
+				startAsync={this.loadCustomFont}
+				onFinish={() => this.setState({ showLoadingScreen: false })}
+			/>
+		)
+	}
+
+	render = () => {
+		let resultPage
+		if (this.state.showLoadingScreen) {
+			resultPage = this.loadingScreen()
+		} else {
+			resultPage = <Auth />
+		}
+		return resultPage
 	}
 }
--- src/commonStyles.js
+++ src/commonStyles.js
@@ -1,13 +1,3 @@
-import * as Font from 'expo-font';
-
-const loadCustomFont = async () => {
-	await Font.loadAsync({
-		'lato': require('../assets/fonts/Lato.ttf'),
-	});
-}
-
-export { loadCustomFont }
-
 export default {
 	fontFamily: 'lato',
 	colors: {
