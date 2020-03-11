474d1cfd3e122c389d4b7fad04a7f94e893f9077
MVRigonati <marcus.rigonati@hotmail.com>
2020-03-10 22:17:07 -0300

    Adicionando campos na tela de login

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
-import { ImageBackground, Text, StyleSheet } from 'react-native'
+import {
+	ImageBackground,
+	Text,
+	StyleSheet,
+	View,
+	TextInput,
+	TouchableOpacity
+} from 'react-native'
 
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
@@ -16,14 +23,59 @@ const styles = StyleSheet.create({
 		color: commonStyles.colors.secondary,
 		fontSize: 70,
 		marginBottom: 10
+	},
+	formContainer: {
+		backgroundColor: 'rgba(0, 0, 0, 0.8)',
+		padding: 20,
+		width: '90%'
+	},
+	input: {
+		marginTop: 10,
+		backgroundColor: '#FFF',
+		padding: 15
+	},
+	button: {
+		backgroundColor: '#080',
+		marginTop: 10,
+		padding: 10,
+		alignItems: 'center'
+	},
+	buttonText: {
+		fontFamily: commonStyles.fontFamily,
+		color: '#FFF',
+		fontSize: 20
 	}
 })
 
 export default class Auth extends Component {
+	state = {
+		email: '',
+		password: ''
+	}
+
 	render() {
 		return (
 			<ImageBackground source={backgroundImage} style={styles.background}>
 				<Text style={styles.title}>Tasks</Text>
+				<View style={styles.formContainer}>
+					<TextInput
+						placeholder="E-mail"
+						value={this.state.email}
+						style={styles.input}
+						onChangeText={email => this.setState({ email })}
+					/>
+					<TextInput
+						placeholder="Senha"
+						value={this.state.password}
+						style={styles.input}
+						onChangeText={password => this.setState({ password })}
+					/>
+					<TouchableOpacity>
+						<View style={styles.button}>
+							<Text style={styles.buttonText}>Entrar</Text>
+						</View>
+					</TouchableOpacity>
+				</View>
 			</ImageBackground>
 		)
 	}
