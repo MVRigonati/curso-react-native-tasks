86a01abe2ee1d1b20151b303335876ac7a797894
MVRigonati <marcus.rigonati@hotmail.com>
2020-03-14 18:59:01 -0300

    Adicionando troca de estado entre Sign-In e Sign-Up

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -24,6 +24,13 @@ const styles = StyleSheet.create({
 		fontSize: 70,
 		marginBottom: 10
 	},
+	subtitle: {
+		fontFamily: commonStyles.fontFamily,
+		color: '#FFF',
+		fontSize: 20,
+		textAlign: 'center',
+		marginBottom: 10
+	},
 	formContainer: {
 		backgroundColor: 'rgba(0, 0, 0, 0.8)',
 		padding: 20,
@@ -49,15 +56,56 @@ const styles = StyleSheet.create({
 
 export default class Auth extends Component {
 	state = {
+		name: '',
 		email: '',
-		password: ''
+		password: '',
+		confirmPassword: '',
+		stageNew: false
+	}
+
+	singinSignup = () => {
+		if (this.state.stageNew) {
+			// Registrar user
+		} else {
+			// Logar user
+		}
 	}
 
 	render() {
+		let showNameInput
+		let showConfirmPasswordInput
+		if (this.state.stageNew) {
+			showNameInput = (
+				<TextInput
+					placeholder="Nome"
+					value={this.state.name}
+					style={styles.input}
+					onChangeText={name => this.setState({ name })}
+				/>
+			)
+			showConfirmPasswordInput = (
+				<TextInput
+					placeholder="Confirme sua senha"
+					secureTextEntry={true}
+					value={this.state.confirmPassword}
+					style={styles.input}
+					onChangeText={confirmPassword =>
+						this.setState({ confirmPassword })
+					}
+				/>
+			)
+		}
+
 		return (
 			<ImageBackground source={backgroundImage} style={styles.background}>
 				<Text style={styles.title}>Tasks</Text>
 				<View style={styles.formContainer}>
+					<Text style={styles.subtitle}>
+						{this.state.stageNew
+							? 'Crie sua conta'
+							: 'Informe seus dados'}
+					</Text>
+					{showNameInput}
 					<TextInput
 						placeholder="E-mail"
 						value={this.state.email}
@@ -66,16 +114,31 @@ export default class Auth extends Component {
 					/>
 					<TextInput
 						placeholder="Senha"
+						secureTextEntry={true}
 						value={this.state.password}
 						style={styles.input}
 						onChangeText={password => this.setState({ password })}
 					/>
-					<TouchableOpacity>
+					{showConfirmPasswordInput}
+					<TouchableOpacity onPress={this.singinSignup}>
 						<View style={styles.button}>
-							<Text style={styles.buttonText}>Entrar</Text>
+							<Text style={styles.buttonText}>
+								{this.state.stageNew ? 'Registrar' : 'Entrar'}
+							</Text>
 						</View>
 					</TouchableOpacity>
 				</View>
+				<TouchableOpacity
+					style={{ padding: 10 }}
+					onPress={() =>
+						this.setState({ stageNew: !this.state.stageNew })
+					}>
+					<Text style={styles.buttonText}>
+						{this.state.stageNew
+							? 'Já possui conta?'
+							: 'Ainda não possui conta?'}
+					</Text>
+				</TouchableOpacity>
 			</ImageBackground>
 		)
 	}
