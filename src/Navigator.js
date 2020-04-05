0b9b53e7220dd6e977f59f7ddcbb1698e746e770
MVRigonati <marcus.rigonati@hotmail.com>
2020-04-05 00:47:36 -0300

    Adicionando menu lateral - parte 1

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,8 +1,43 @@
+import React from 'react'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 
 import { screenRoutes } from './common'
 import Auth from './screens/Auth'
 import TaskList from './screens/Agenda'
+
+const sideMenuRoutes = {
+	Today: {
+		name: 'today',
+		screen: props => <TaskList {...props} title="Hoje" daysAhead={0} />,
+		navigationOptions: {
+			title: 'Hoje'
+		}
+	},
+	Tomorrow: {
+		name: 'tomorrow',
+		screen: props => <TaskList {...props} title="Amanhã" daysAhead={1} />,
+		navigationOptions: {
+			title: 'Amanhã'
+		}
+	},
+	Week: {
+		name: 'week',
+		screen: props => <TaskList {...props} title="Semana" daysAhead={7} />,
+		navigationOptions: {
+			title: 'Semana'
+		}
+	},
+	Month: {
+		name: 'month',
+		screen: props => <TaskList {...props} title="Mês" daysAhead={30} />,
+		navigationOptions: {
+			title: 'Mês'
+		}
+	}
+}
+
+const sideMenuNavigator = createDrawerNavigator(sideMenuRoutes)
 
 const MainRoutes = {
 	auth: {
@@ -11,7 +46,7 @@ const MainRoutes = {
 	},
 	home: {
 		name: screenRoutes.home,
-		screen: TaskList
+		screen: sideMenuNavigator
 	}
 }
 
