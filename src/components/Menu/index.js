2ede018b181742d0f0617192dbd151cfcc88e55d
MVRigonati <marcus.rigonati@hotmail.com>
2020-04-21 14:44:24 -0300

    Alterando informacoes do usuario

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,25 +1,14 @@
 import React from 'react'
-import { ScrollView, View, StyleSheet, Text } from 'react-native'
+import { ScrollView, View, Text } from 'react-native'
 import { Gravatar } from 'react-native-gravatar'
 import { DrawerItems } from 'react-navigation-drawer'
 
-const styles = StyleSheet.create({
-	header: {
-		borderBottomWidth: 1,
-		borderColor: '#DDD'
-	},
-	avatar: {
-		height: 60,
-		width: 60,
-		borderWidth: 3,
-		borderRadius: 30,
-		margin: 50
-	}
-})
+import styles from './styles'
 
 export default props => (
 	<ScrollView>
 		<View style={styles.header}>
+			<Text style={styles.title}>Tasks</Text>
 			<Gravatar
 				style={styles.avatar}
 				options={{
@@ -27,9 +16,9 @@ export default props => (
 					secure: true
 				}}
 			/>
-			<View style={styles.userInfo}>
-				<Text>{props.navigation.getParam('name')}</Text>
-				<Text>{props.navigation.getParam('email')}</Text>
+			<View>
+				<Text style={styles.name}>{props.navigation.getParam('name')}</Text>
+				<Text style={styles.email}>{props.navigation.getParam('email')}</Text>
 			</View>
 		</View>
 		<DrawerItems {...props} />
---
+++ src/components/Menu/styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native'
+
+import commonStyles from '../../commonStyles'
+
+const styles = StyleSheet.create({
+	header: {
+		borderBottomWidth: 1,
+		borderColor: '#DDD',
+		paddingLeft: 10
+	},
+	title: {
+		color: '#000',
+		fontFamily: commonStyles.fontFamily,
+		fontSize: 30,
+		paddingTop: 50,
+		marginBottom: 15
+	},
+	avatar: {
+		height: 60,
+		width: 60,
+		borderWidth: 3,
+		borderRadius: 30,
+		marginBottom: 10
+	},
+	name: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.mainText,
+		fontSize: 20,
+		marginBottom: 5
+	},
+	email: {
+		fontFamily: commonStyles.fontFamily,
+		color: commonStyles.colors.subTex,
+		fontSize: 15,
+		marginBottom: 10
+	}
+})
+
+export default styles
